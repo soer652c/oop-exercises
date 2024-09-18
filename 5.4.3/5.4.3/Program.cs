f0b2fea37ee75cf189f67e8bb11d7b60e22a4e91
Søren Bomberg <soer652c@gmail.com>
2024-09-18 14:11:59 +0200

    Opgaver

---
+++ 5.4.3/5.4.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Intmaskine{
+static void Main(String[] args)
+{
+    int maxValue = int.MaxValue;
+    Console.WriteLine("Maksimal værdi for int: " + maxValue);
+
+    int overflowValue = maxValue + 1;
+    Console.WriteLine("Efter overflow: " + overflowValue);
+
+    Console.WriteLine("Minimale værdi for int: " + int.MinValue);
+}
+}
